f7743661b6d037a30a11f522d791bca1d0bfabb8
LaMothe <Todd.LaMothe@allscripts.com>
2016-09-08 13:34:38 -0400

    Added API project shell

---
+++ beerdb/Models/Brewery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace beerdb.Models
+{
+    public class Brewery
+    {
+        public int totalResults { get; set; }
+    }
+}

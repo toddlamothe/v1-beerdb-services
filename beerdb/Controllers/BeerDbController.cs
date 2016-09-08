9a368d851769d7bc56f62cd251708b4b0f1440d7
LaMothe <Todd.LaMothe@allscripts.com>
2016-09-08 13:43:30 -0400

    Added rest API controller

--- beerdb/Controllers/BeerDbController.cs
+++ beerdb/Controllers/BeerDbController.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using beerdb.Models;
 
 namespace beerdb.Controllers
 {
     public class BeerDbController : ApiController
     {
+        Brewery brewery = new Brewery { totalResults = 1 };
+
+        public Brewery getBrewery()
+        {
+            return brewery;
+        }
     }
 }
